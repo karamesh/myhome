8e4d1ac7457b53b2e980c492d8f02353f0c42f70
karamesh <karthikramesh@icloud.com>
2013-08-12 11:30:20 +0530

    Modified the web.js file

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send('karthik ramesh');
+	var message = fs.readFileSync("index.html").toString()
+	    response.send(message);
     });
 
-var port = process.env.PORT || 5000;
+app.use("/Scripts", express.static(Scripts)
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
     });
