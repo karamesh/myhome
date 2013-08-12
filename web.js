389e389f873a74872d61edb5e959c19d8b6f3aeb
karamesh <karthikramesh@icloud.com>
2013-08-12 11:47:22 +0530

    Modified the web.js file

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
 	reponse.send("Hello World")
     });
 
-app.use("/Scripts", express.static(Scripts))
+app.use("/Scripts", express.static(Scripts + "/"))
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
