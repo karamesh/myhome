3efc2a58bb052347801c23a0c662dae4ab8344c6
karamesh <karthikramesh@icloud.com>
2013-08-12 11:55:40 +0530

    Modified the web.js file

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	//	var message = fs.readFileSync("index.html").toString()
-	//    response.send(message);
-	reponse.send("Hello World")
+	var message = fs.readFileSync("index.html").toString()
+	response.send(message);
+	
     });
 
-app.use("/Scripts", express.static(Scripts + "/"))
+//app.use("/Scripts", express.static(Scripts + "/"))
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
